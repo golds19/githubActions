2b4544bf00323675fe87c9ca18d1a3d50e8dc27b
golds19 <gsbensaint19@gmail.com>
2025-05-14 04:13:35 +0900

    updated unit tests

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,9 +2,9 @@ from src.math_operations import add, sub
 
 def test_add():
     assert add(2,3) == 5
-    assert add(0,0) == 0
+    assert add(1,1) == 2
 
 def test_sub():
     assert sub(5,3) == 2
-    assert sub(0,0) == 0
-    assert sub(3,5) == -2
+    assert sub(5,2) == 3
+    assert sub(10,9) == 1
